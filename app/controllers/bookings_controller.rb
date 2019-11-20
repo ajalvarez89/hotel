55292a4e740c42163c98660b5a2c8dc2cec14d12
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-11-20 11:20:38 -0500

    On branch aa-config-bin-setup  Changes to be committed: 	modified:   app/controllers/bookings_controller.rb 	modified:   db/seeds.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,41 +4,41 @@ class BookingsController < ApplicationController
 
       if @booking.present?
         render json: @booking.includes(:room,:user), status: :ok
-      else 
+      else
         render json: { error: 'No data was found' }, status: :unprocessable_entity
       end
     end
-  
+
     def show
       @booking = Booking.find(params[:id])
       render json: @booking, status: :ok
     end
-  
+
     def create
       booking = Booking.new(booking_params)
-  
+
       if booking.save
         render json: booking, status: :created
-      else 
+      else
         render json: booking.errors, status: :unprocessable_entity
       end
     end
 
-    def update 
+    def update
       booking = Booking.find(params[:id])
       booking.assign_attributes(minibar: booking_params[:minibar])
 
       if booking.save
         booking.calculate_final_price
         render json: booking, status: :ok
-      else 
+      else
         render json: booking.errors, status: :unprocessable_entity
       end
     end
 
-    private 
+    private
 
-    def booking_params 
+    def booking_params
       params.require(:booking).permit(:room_id, :user_id, :check_in, :check_out, :minibar)
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+puts '== Installing Seeds =='
 # add rooms
+puts '***** Creating Rooms'
 Room.create(number: '101', daily_rate: 50000)
 Room.create(number: '102', daily_rate: 75000)
 Room.create(number: '201', daily_rate: 55000)
@@ -14,6 +15,7 @@ Room.create(number: '202', daily_rate: 65000)
 Room.create(number: '301', daily_rate: 80000)
 
 # add customers
+puts '***** Creating Users'
 User.create(first_name: 'Alvaro', last_name: 'Alvarez', loyalty_member_tier: 'bronze')
 User.create(first_name: 'Grecia', last_name: 'Contreras', loyalty_member_tier: 'silver')
 User.create(first_name: 'Lady', last_name: 'Bernal', loyalty_member_tier: 'gold')
